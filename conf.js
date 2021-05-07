44438b9ca80f4d45a904b5955fddee5571ea14dc
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-05-07 12:41:45 +0100

    Config update for Promise handling

--- conf.js
+++ conf.js
@@ -11,7 +11,7 @@ exports.config = {
      */
     //seleniumServerJar: "node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.4.0.jar",
     directConnect: true,
-    SELENIUM_PROMISE_MANAGER: false,
+    SELENIUM_PROMISE_MANAGER: true,
 
     specs: ['specs/*Spec.js'],
     baseUrl: 'https://www.saucedemo.com',
