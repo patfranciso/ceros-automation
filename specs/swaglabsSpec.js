e80daa406b26fac1645778593cd827a216e6737c
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-05-09 21:22:09 +0100

    Restored beforeAll in setup

--- specs/swaglabsSpec.js
+++ specs/swaglabsSpec.js
@@ -3,19 +3,16 @@ import * as Assert from "../framework/assertion.js";
 import I from "../pages/AcceptanceTester.js";
 
 describe ('Swag Labs tests', () => {
-    beforeEach(async () => {
+    beforeAll(async () => {
         await I.goto(browser.baseUrl);
+        I.login();
     });
 
     it('should log in with standard user', async ()=> {
-        I.login();
-
         Assert.currentUrlIs('/inventory.html');
     });
 
     it('should add an item to the cart', async () => {
-        I.login();
-
         I.click('#add-to-cart-sauce-labs-bolt-t-shirt');
         I.click('#add-to-cart-sauce-labs-backpack');
 
@@ -24,14 +21,10 @@ describe ('Swag Labs tests', () => {
     });
 
     it('should have 6 items on the inventory page', async () => {
-        I.login();
-
         Assert.itemCount('.inventory_item', 6);
     });
 
     it('should complete the purchase process of an item from the inventory', async () => {
-        I.login();
-
         I.click('.btn_inventory 2');
         I.click('.btn_inventory 3');
         I.click('.btn_inventory 4');
@@ -64,8 +57,7 @@ describe ('Swag Labs tests', () => {
 
     // BONUS tests! Not required for the automation challenge, but do these if you can.
     it('sort the inventory items by price, high-to-low', async () => {
-        I.login();
-
+        I.goto('/inventory.html');
         const fn = text => Number(text.substr(1));
         const unorderedPrices = await I.map('.inventory_item_price', fn);
         const orderedPrices = unorderedPrices.sort((a,b) => b - a);
@@ -77,8 +69,7 @@ describe ('Swag Labs tests', () => {
     });
 
     it('sort the inventory items by name, Z-to-A', async () => {
-        I.login();
-
+        I.goto('/inventory.html');
         const fn = text => text ;
         const unorderedNames = await I.map('.inventory_item_name', fn);
         const orderedNames = unorderedNames.sort().reverse();
